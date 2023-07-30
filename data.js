c41ddbf4f3d865e8cc8f7640f159d31c6ea09547
Shreyasi Chowdhury <85079050+ShreyasiChowdhury@users.noreply.github.com>
2023-07-30 22:11:54 +0530

    First Commit

---
+++ data.js
@@ -0,0 +1,115 @@
+export const deal = [
+
+    {
+        img: "https://m.media-amazon.com/images/I/61SSVxTSs3L._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/71l9xxzqg0L._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61qJaW4GrEL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/71lYSEETh5L._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61y2VVWcGBL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61w8sH5L+oL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/71tH69LXCML._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61TapeOXotL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61SbNsTVBeL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61JyHJWUf-L._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61jQim6YPnL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61kaqYoix4L._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/71H-dH3u4jL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61v37OZPVtL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61EzUZG0njL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/6151jBIPv8L._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61X9wnaI6kL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61gRRiSF2iL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/71PoXSejZML._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61pdxkS5WdL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61sSX4NWy5L._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/619npqOI7aL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61xWEQ4ZTFL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61dX3qRMt9L._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61DZclqQ4RL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61akt30bJsL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61pBjsIyVeL._AC_SY200_.jpg"
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61S9aVnRZDL._AC_SY200_.jpg"
+    }
+
+]
